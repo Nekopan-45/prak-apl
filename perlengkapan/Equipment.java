2f455cf3b9c0701d48af61494b7c83d4e9064f5c
Nekopan-45 <74187496+Nekopan-45@users.noreply.github.com>
2021-04-12 09:21:30 +0800

    Update Equipment.java

--- perlengkapan/Equipment.java
+++ perlengkapan/Equipment.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perlengkapan;
 
-/**
- *
- * @author Acer Gk
- */
 public class Equipment {
     private String paket;
     protected String merk;
